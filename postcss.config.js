14f093d401b7d5e00f5eedd5160acf9a8f8078fa
Ryan Yao <hello@ryanyao.design>
2020-05-19 11:40:15 -0700

    Change to SimpleAnalyrics

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,20 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
-const cssnano = require("cssnano");
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
 
 module.exports = {
   plugins: [
-    require("tailwindcss"),
+    require('tailwindcss'),
     require('postcss-nested'),
-    require("autoprefixer"),
+    require('autoprefixer'),
     cssnano({
-      preset: "default"
+      preset: 'default'
     }),
     purgecss({
       content: [
-        "./src/*.html",
-        './node_modules/tailwindcss-dark-mode/prefers-dark.js',
+        './src/*.html',
+        './node_modules/tailwindcss-dark-mode/prefers-dark.js'
       ],
       defaultExtractor: content => content.match(/[\w-/:]*[\w-/:]/g) || []
     })
   ]
-};
+}
